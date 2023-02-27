0c0be4fedf0fc72242b02ba87135899da7f3bd8a
Aaron Rodrigues <38214417+aaronjrodrigues@users.noreply.github.com>
2023-02-27 19:21:03 +0200

    Add files via upload

---
+++ BubbleSort/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+    Sub Main()
+        Dim Array() As Integer = {25, 27, 3, 4, 8, 2, 0, 35, 47, 2}
+        Call BubbleSort(Array)
+    End Sub
+
+    Sub BubbleSort(Array() As Integer)
+        Dim Counter, Counter1, Temp, Temp1 As Integer
+        Dim Sorted As Boolean
+
+        For Counter1 = 0 To 999
+            For Counter = 0 To 8
+                Sorted = False
+                If Array(Counter) > Array(Counter + 1) Then
+                    Temp = Array(Counter)
+                    Temp1 = Array(Counter + 1)
+                    Array(Counter) = Temp1
+                    Array(Counter + 1) = Temp
+                    Sorted = True
+                End If
+            Next
+        Next
+
+        Console.WriteLine("Outputing the sorted loop")
+
+        For Counter = 0 To 9
+            Console.WriteLine(Array(Counter))
+        Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ BubbleSort/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("BubbleSort")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("BubbleSort")> 
+<Assembly: AssemblyCopyright("Copyright ©  2023")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4f4d239d-41c2-4efe-8f8c-ccae561da7f1")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
